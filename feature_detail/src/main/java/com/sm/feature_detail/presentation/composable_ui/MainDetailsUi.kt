d4f9c04df5fc45876d7ccc60735e881f374f7205
Sergio Mendoza <srgmendoza@gmail.com>
2023-10-05 11:56:45 +0200

    Navigating between modules
    Navigating without BottomBar
    Navigating with params
    Getting params on destination view

--- feature_detail/src/main/java/com/sm/feature_detail/presentation/composable_ui/MainDetailsUi.kt
+++ feature_detail/src/main/java/com/sm/feature_detail/presentation/composable_ui/MainDetailsUi.kt
@@ -2,10 +2,10 @@ package com.sm.feature_detail.presentation.composable_ui
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 
 @Composable
 fun MainDetailsUi(navController: NavHostController) {
-    Text(text = "Welcome to DetailsUi")
+    val id = navController.currentBackStackEntry?.arguments?.getString("id")
+    Text(text = "Welcome to DetailsUi for character with id: $id")
 }
