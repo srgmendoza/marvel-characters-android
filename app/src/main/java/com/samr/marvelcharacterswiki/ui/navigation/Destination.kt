994486f7349b1632a35f8e120984c62008970030
Sergio Mendoza <srgmendoza@gmail.com>
2023-10-05 10:46:36 +0200

    Navigating between bottom tabs

--- app/src/main/java/com/samr/marvelcharacterswiki/ui/navigation/Destination.kt
+++ app/src/main/java/com/samr/marvelcharacterswiki/ui/navigation/Destination.kt
@@ -2,8 +2,10 @@ package com.samr.marvelcharacterswiki.ui.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Destination(val name: String, val path: String, val icon: ImageVector) {
-    object Listing : Destination(name = "Listing", path = "listing", icon = Icons.Default.List)
+    data object Listing : Destination(name = "Listing", path = "listing", icon = Icons.Default.List)
+    data object Search : Destination(name = "Search", path = "search", icon = Icons.Default.Search)
 }
--- feature_search/src/main/java/com/sm/feature_search/presentation/di/UiModule.kt
+++ feature_search/src/main/java/com/sm/feature_search/presentation/di/UiModule.kt
@@ -1,5 +1,9 @@
 package com.sm.feature_search.presentation.di
 
+import com.sm.feature_search.SearchFeatureImpl
+import com.sm.feature_search_api.SearchFeatureApi
 import org.koin.dsl.module
 
-internal val presentationModule = module {  }
+internal val presentationModule = module {
+    single<SearchFeatureApi> { SearchFeatureImpl() } //Exposing navigation to the DI system
+}
